b270f1508e624b2e7954324d4b601c48204612ca
Olia <olga.shostak1991@gmail.com>
2020-07-02 09:35:52 +0300

    changes to the start button

--- index.js
+++ index.js
@@ -1,42 +1,39 @@
-"use strict";
+'use strict';
 const colors = [
-  "#FFFFFF",
-  "#2196F3",
-  "#4CAF50",
-  "#FF9800",
-  "#009688",
-  "#795548",
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
 ];
 
 const refs = {
-  body: document.querySelector("body"),
-  startBtn: document.querySelector('button[data-action="start"]'),
-  stopBtn: document.querySelector('button[data-action="stop"]'),
+  body: document.querySelector('body'),
+  start: document.querySelector('button[data-action="start"]'),
+  stop: document.querySelector('button[data-action="stop"]'),
 };
-
-const colorSwitch = {
-  isActive: false,
-  start() {
-    if (this.isActive) {
-      return;
-    }
-    this.isActive = true;
-    this.colorSwitchBtnId = setInterval(randomBodyColor, 1000);
-  },
-  stop() {
-    clearInterval(this.colorSwitchBtnId);
-    this.isActive = false;
-  },
-};
-
-refs.startBtn.addEventListener("click", colorSwitch.start.bind(colorSwitch));
-refs.stopBtn.addEventListener("click", colorSwitch.stop.bind(colorSwitch));
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-function randomBodyColor() {
-  refs.body.style.backgroundColor =
-    colors[randomIntegerFromInterval(0, colors.length)];
-}
+let intervalId;
+
+const changeColor = () => {
+  let color = randomIntegerFromInterval(0, colors.length - 1);
+  refs.body.bgColor = colors[color];
+};
+
+const startHandler = () => {
+  refs.start.disabled = true;
+  intervalId = setInterval(changeColor, 1000);
+};
+
+const stopHandler = () => {
+  clearInterval(intervalId);
+  refs.start.disabled = false;
+};
+
+refs.start.addEventListener('click', startHandler);
+refs.stop.addEventListener('click', stopHandler);
